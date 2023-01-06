0d1d124eb53fd68a0a054702c0c9347e1484ed54
Suneel Varma <49975815+Suneelvarma5fi@users.noreply.github.com>
2023-01-06 14:35:56 +0530

    Fixing CCN

--- breach_range.py
+++ breach_range.py
@@ -5,9 +5,9 @@ def get_breach_range(value, ulimit,llimit):
 		return 1
 	elif value > ulimit:
 		return 5
-	elif value == llimit or value <= llimit+warning_limit:
+	elif (value == llimit or value <= llimit+warning_limit):
 		return 2
-	elif value == ulimit or value <= llimit-warning_limit:
+	elif (value == ulimit or value <= llimit-warning_limit):
 		return 4
 	else:
 		return 3
