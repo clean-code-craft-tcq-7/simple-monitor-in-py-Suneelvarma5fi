d9e7d7dd586c8b9f473c560a6ecf6e93f1fc11d9
Suneel Varma <49975815+Suneelvarma5fi@users.noreply.github.com>
2023-01-06 14:20:51 +0530

    Fixed an error

--- battery_is_ok.py
+++ battery_is_ok.py
@@ -3,4 +3,4 @@ from check_soc import check_soc
 from check_chargerate import check_chargerate
 
 def battery_is_ok(temp,soc,chargerate,tunit):
-	return AND(check_temperature(temp,tunit),check_soc(soc),check_chargerate(chargerate))
+	return ALL(check_temperature(temp,tunit),check_soc(soc),check_chargerate(chargerate))
