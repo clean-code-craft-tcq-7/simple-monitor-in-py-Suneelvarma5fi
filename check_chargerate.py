e9368113c7fd62e7ee5bfa43222eb89cb0d3346c
Suneel Varma <49975815+Suneelvarma5fi@users.noreply.github.com>
2023-01-06 14:52:41 +0530

    Fixed an Error

--- check_chargerate.py
+++ check_chargerate.py
@@ -6,4 +6,4 @@ def check_chargerate(CR):
 	breach_range_value = get_breach_range(CR,upperLimit,lowerLimit)
 	message = MESSAGES[breach_range_value]
 	print(message)
-	return !(breach_range_value == 1 or breach_range_value == 5)
+	return not (breach_range_value == 1 or breach_range_value == 5)
